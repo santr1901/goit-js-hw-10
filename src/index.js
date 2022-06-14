bda47ac3098023131740aea612b176a968eb816f
Dmitrij Sanator <32552584+santr1901@users.noreply.github.com>
2022-06-14 15:19:01 +0300

    Summary

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+
+const search = document.querySelector("#search-box");
+const countryList = document.querySelector(".country-list");
+const info = document.querySelector(".country-info");
 
 const DEBOUNCE_DELAY = 300;
+
+
+search.addEventListener("input", debounce((readInput), DEBOUNCE_DELAY));
+
+function readInput(event) {
+    const inputCountryName = event.target.value;
+
+    fetchCountries(inputCountryName)
+        .then(data => {
+            if (data.length === 1) {
+                clearList();
+                countryCard(data) 
+            } else if (data.length > 1 && data.length <= 10) {
+                clearInfo();
+                countriesList(data) 
+            } else if (data.length > 10) {
+                Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+            }    
+                     
+        })
+}
+
+function countriesList(data) {
+ const markupCountriesList = data.map(({ name, flags }) =>
+    `<li><img src=${flags.svg} width=40px height=20px>
+    <h2>${name.official}</h2>
+        </li>`
+    ).join("");
+    
+    countryList.innerHTML = markupCountriesList;
+}
+
+function countryCard(data) {
+    const markupCountryCard = data.map(({ name, capital, population, languages, flags }) =>
+        `<div><img src=${flags.svg} width=40px height=20px>
+    <h2>${name.official}</h2>
+        </div>
+    <p>Capital: ${capital}</p>
+    <p>Population: ${population}</p>
+    <p>Language: ${Object.values(languages)}</p>`
+    ).join("");
+    
+    info.innerHTML = markupCountryCard;
+}
+    
+function clearInfo() {
+    info.innerHTML = "";
+}
+
+function clearList() {
+    countryList.innerHTML = ""
+}
