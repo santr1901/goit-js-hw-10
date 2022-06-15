babf13add7f6e44db7a1dc276f4309c9cf8c7638
Dmitrij Sanator <32552584+santr1901@users.noreply.github.com>
2022-06-15 15:09:38 +0300

    countrySearch-v1.0

--- src/index.js
+++ src/index.js
@@ -7,47 +7,60 @@ const search = document.querySelector("#search-box");
 const countryList = document.querySelector(".country-list");
 const info = document.querySelector(".country-info");
 
+
 const DEBOUNCE_DELAY = 300;
 
 
 search.addEventListener("input", debounce((readInput), DEBOUNCE_DELAY));
 
 function readInput(event) {
-    const inputCountryName = event.target.value;
+    const inputCountryName = event.target.value.trim();
 
+    if (inputCountryName === "") {
+        clearList();
+        return;
+    } 
+    
     fetchCountries(inputCountryName)
         .then(data => {
-            if (data.length === 1) {
+                if (data.length === 1) {
                 clearList();
                 countryCard(data) 
             } else if (data.length > 1 && data.length <= 10) {
                 clearInfo();
+                clearList();
                 countriesList(data) 
             } else if (data.length > 10) {
                 Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
-            }    
+            } 
                      
-        })
+        }).catch(error => {
+            console.log(error)
+          Notiflix.Notify.failure("Oops, there is no country with that name");  
+      
+    })
 }
 
 function countriesList(data) {
  const markupCountriesList = data.map(({ name, flags }) =>
-    `<li><img src=${flags.svg} width=40px height=20px>
-    <h2>${name.official}</h2>
+    `<li class="country-item" ><img src=${flags.svg} width=30px height=15px>
+    <b>${name.official}</b>
         </li>`
     ).join("");
-    
-    countryList.innerHTML = markupCountriesList;
+   
+    // countryList.innerHTML = markupCountriesList;
+    countryList.insertAdjacentHTML("afterbegin", markupCountriesList)
+    countryList.style.listStyle = "none";
 }
 
 function countryCard(data) {
     const markupCountryCard = data.map(({ name, capital, population, languages, flags }) =>
         `<div><img src=${flags.svg} width=40px height=20px>
-    <h2>${name.official}</h2>
+    <strong>${name.official}</strong>
         </div>
-    <p>Capital: ${capital}</p>
-    <p>Population: ${population}</p>
-    <p>Language: ${Object.values(languages)}</p>`
+    <p><strong>Capital:</strong> ${capital}</p>
+    <p><strong>Population:</strong> ${population}</p>
+    <p><strong>Language:</strong> ${Object.values(languages)}</p>`
     ).join("");
     
     info.innerHTML = markupCountryCard;
